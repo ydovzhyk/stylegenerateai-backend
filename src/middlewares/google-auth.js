7fb79d4cf1b8cb18b2903bf14641cf2cdac6d699
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2026-04-07 16:47:11 +0300

    added corrections

--- src/middlewares/google-auth.js
+++ src/middlewares/google-auth.js
@@ -2,13 +2,12 @@ const passport = require('passport')
 const { Strategy } = require('passport-google-oauth2')
 const bcrypt = require('bcrypt')
 const shortid = require('shortid')
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, BASE_URL_HEROKU, BASE_URL } =
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, BASE_URL_HEROKU } =
   process.env
 
 const { User } = require('../models/user.model')
 
-const callbackURL =
-process.env.NODE_ENV === 'production' ? `${BASE_URL_HEROKU}/api/google/callback` : `${BASE_URL}/api/google/callback`
+const callbackURL = `${BASE_URL_HEROKU}/api/google/callback`
 
 const googleParams = {
   clientID: GOOGLE_CLIENT_ID,
